9318d44b4642c51fbe6082e6d2b962d08e14104a
Robot <alightinastorm@icloud.com>
2026-03-21 20:17:49 +0100

    feat: remove anim-editor-react package and its related files

--- packages/anim-editor-react/README.md
+++
@@ -1,8 +0,0 @@
-# @ggez/anim-editor-react
-
-React hooks and components for rendering and editing animation graphs on top of `@ggez/anim-editor-core`.
-
-Main exports:
-
-- `useEditorStoreValue`
-- `AnimationEditorWorkspace`
--- packages/anim-editor-react/src/hooks.js
+++
@@ -1,4 +0,0 @@
-import { useSyncExternalStore } from "react";
-export function useEditorStoreValue(store, selector, topics = ["document"]) {
-    return useSyncExternalStore((listener) => store.subscribe(listener, topics), selector, selector);
-}
