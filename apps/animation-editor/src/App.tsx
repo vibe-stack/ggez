64d2ac56117726a5a7bf2af94b1651b002c2cfbf
Robot <alightinastorm@icloud.com>
2026-03-21 14:44:04 +0100

    feat: update App component and styles for improved layout and design

--- apps/animation-editor/src/App.tsx
+++ apps/animation-editor/src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <h1 className="text-3xl font-bold">Hello World!</h1>
+    <h1>Hello World!</h1>
   )
 }
 
--- apps/animation-editor/src/main.tsx
+++ apps/animation-editor/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './styles.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles.css";
+import { TooltipProvider } from "./components/ui/tooltip";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <TooltipProvider>
+      <App />
+    </TooltipProvider>
   </StrictMode>,
-)
+);
